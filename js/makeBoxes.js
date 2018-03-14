e6374ea1bf7a03aacb5685d799581aa0da25d0fa
Jason Seminara <jseminara@gmail.com>
2018-03-13 23:01:45 -0400

    added eslint, gitignore; moved styles and js into sep files

---
+++ js/makeBoxes.js
@@ -0,0 +1,37 @@
+/* eslint-env browser */
+const section = document.querySelector('section');
+
+const rnd = num => Math.floor(Math.random() * num);
+let currentColor = rnd(255);
+
+// when the click event finally propagates here,
+// and swap out the color
+section.addEventListener('click', () => {
+  currentColor = rnd(255);
+});
+
+// make n boxes and keep track of their id as i
+const makeBox = (n, i) => {
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  const flipColor = () => {
+    box.style.backgroundColor = `hsla(${currentColor}, 80%, 20%, .17)`;
+  };
+
+  // The click event propagates in near-realtime (dumb humans)
+  // Let's set a timeout so your inferior eyes can see it.
+  box.addEventListener('click', () => setTimeout(flipColor, 200 - (20 * i)));
+  return box;
+};
+
+// Make 25 boxes
+const boxes = [...Array(25)].map(makeBox)
+  .reverse()
+  .reduce((b, a) => {
+    a.appendChild(b);
+    return a;
+  });
+
+// append the boxes to the section
+section.appendChild(boxes);
